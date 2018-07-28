19482ec1cf419c34039508d79bb382d00321041e
rabibsust <rabib.sust@gmail.com>
2018-07-29 01:28:46 +0600

    Transition to a second view controller

--- Plain Notes/ViewController.swift
+++ Plain Notes/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         data.insert(name, at: 0)
         let indexPath:IndexPath = IndexPath(row: 0, section: 0)
         table.insertRows(at: [indexPath], with: .automatic)
-        save()
+        self.performSegue(withIdentifier: "detail", sender: nil)
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -63,7 +63,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("\(data[indexPath.row])")
+        //print("\(data[indexPath.row])")
+        self.performSegue(withIdentifier: "detail", sender: nil)
     }
     
     func save() {
